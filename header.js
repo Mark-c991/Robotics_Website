58747d726a5237e168672339d16b1d3321ac7fc6
ArmorRobotics <116299717+ArmorRobotics@users.noreply.github.com>
2022-10-28 22:31:45 -0700

    js file with header html

---
+++ header.js
@@ -0,0 +1,41 @@
+document.write(`
+    <header>
+        <div class="headerimg">
+            <img src="images/R_purplegold.png">
+        </div
+        ><div class="headertxt">
+            <h1>Armor Robotics 9143</h1>
+            <h2>Archbishop Riordan High School</h2>
+            
+            <div class="navbar">
+                <a href="#">Home</a
+                ><div class="dropdown"
+                    ><span>About <i class="dropcaret">&#9660;</i></span
+                        ><div class="dropdown-content">
+                        <a href="#">Leadership</a>
+                        <a href="#">Volunteers</a>
+                        <a href="#">Departments</a>
+                        <a href="#">History</a>
+                        <a href="#">Social Media</a
+                    ></div
+                ></div
+                ><div class="dropdown"
+                    ><span>Resources <i class="dropcaret">&#9660;</i></span
+                    ><div class="dropdown-content">
+                        <a href="calendar.html">Calendar</a>
+                        <a href="#">Handbook</a>
+                        <a href="#">Constitution</a
+                    ></div
+                ></div
+                ><a href="#">Sponsors</a
+                ><div class="dropdown"
+                    ><span>Support Us <i class="dropcaret">&#9660;</i></span
+                    ><div class="dropdown-content">
+                        <a href="#">Donate</a>
+                        <a href="#">Volunteer</a
+                    ></div
+                ></div>
+            </div>
+        </div>
+    </header>
+`);
